2fbc91e2d1b289daa2e7234ab7aa6d9440b161e9
yaser676 <yaser676@gmail.com>
2014-04-14 14:16:33 +0300

    Update TestWhatsProt.php

--- tests/TestWhatsProt.php
+++ tests/TestWhatsProt.php
@@ -4,7 +4,7 @@ require_once '../src/php/whatsprot.class.php';
 /**
  * Used to expose protected methods to the testing framework.
  */
-class TestWhatsProt extends WhatsProt {
+class TestWhatsProt extends WhatsProt {966530798650
     public function processInboundDataNode(ProtocolNode $node) {
         parent::processInboundDataNode($node);
     }
